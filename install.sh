35833b0e3fdfe56fed2b5754eff0532d51ae675e
Petr Brazdil <p.brazdil@gmail.com>
2014-03-02 12:43:47 +0100

    selinux check added

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+if [ `getenforce`!="Enforcing" ]; then
+  echo -e "SELINUX=enforcing\nSELINUXTYPE=targeted" > /etc/sysconfig/selinux
+  echo "Selinux conf changed, rebooting machine in 5 sec ..."
+  sleep 6
+  reboot
+else
+  echo "Selinux is OK"
+fi
+
 yum -y install ruby unzip httpd-tools puppet bind augeas
 
 sh <(curl -s http://oo-install.rhcloud.com) -d
